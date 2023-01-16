4bb7e1325146bcb3b69cfd5ac7909bc9cf5216a0
dedenurr <dedenurrahman.git@gmail.com>
2023-01-16 16:05:13 +0700

    membuat module say hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dedenurr/go-say-hello
+
+go 1.19
---
+++ say_hello.go
@@ -0,0 +1,10 @@
+package go_say_hello
+
+func SayHello() string {
+	return "Hello"
+}
+
+/* func main() {
+	value := SayHello()
+	fmt.Println(value)
+} */
